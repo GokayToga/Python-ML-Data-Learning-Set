c813adc23ea67c109f64f3c1d60e97af3e408cde
Gokay Toga <97601328+GokayToga@users.noreply.github.com>
2023-07-01 15:19:54 +0300

    Update learningML.py

--- learningML.py
+++ learningML.py
@@ -212,3 +212,54 @@ OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
 # Ensure all columns have string type
 OH_X_train.columns = OH_X_train.columns.astype(str)
 OH_X_valid.columns = OH_X_valid.columns.astype(str)
+
+
+#pipelines
+
+#Step 1: Define Preprocessing Steps
+# Preprocessing for numerical data, inputing missing data
+# Preprocessing for categorical data, applying one-hot encoding 
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+# Preprocessing for numerical data
+numerical_transformer = SimpleImputer(strategy='constant')
+
+# Preprocessing for categorical data
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+# Bundle preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
+
+#Step 2: Define the Model
+from sklearn.ensemble import RandomForestRegressor
+
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+
+#Step 3: Create and Evaluate the Pipeline
+
+from sklearn.metrics import mean_absolute_error
+
+# Bundle preprocessing and modeling code in a pipeline
+my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('model', model)
+                             ])
+
+# Preprocessing of training data, fit model 
+my_pipeline.fit(X_train, y_train)
+
+# Preprocessing of validation data, get predictions
+preds = my_pipeline.predict(X_valid)
+
+# Evaluate the model
+score = mean_absolute_error(y_valid, preds)
+print('MAE:', score)
