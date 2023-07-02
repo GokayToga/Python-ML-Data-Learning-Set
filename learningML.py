6e5d2aba50ce100950c394ae08cd2763db5d9686
Gokay Toga <97601328+GokayToga@users.noreply.github.com>
2023-07-02 14:26:57 +0300

    Update learningML.py

--- learningML.py
+++ learningML.py
@@ -403,3 +403,42 @@ mae_3 = mean_absolute_error(predictions_3, y_valid)
 
 print("Mean Absolute Error:" , mae_3)
 
+
+#Data Leakage
+#There are two main types of leakage: target leakage and train-test contamination.
+
+#Target leakage occurs when your predictors include data that will not be available at the time you make predictions.
+#Train-test contamination occurs when you aren't careful to distinguish training data from validation data.
+
+
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+
+# Since there is no preprocessing, we don't need a pipeline (used anyway as best practice!)
+my_pipeline = make_pipeline(RandomForestClassifier(n_estimators=100))
+cv_scores = cross_val_score(my_pipeline, X, y, 
+                            cv=5,
+                            scoring='accuracy')
+
+print("Cross-validation accuracy: %f" % cv_scores.mean())
+
+expenditures_cardholders = X.expenditure[y]
+expenditures_noncardholders = X.expenditure[~y]
+
+print('Fraction of those who did not receive a card and had no expenditures: %.2f' \
+      %((expenditures_noncardholders == 0).mean()))
+print('Fraction of those who received a card and had no expenditures: %.2f' \
+      %(( expenditures_cardholders == 0).mean()))
+
+#droping the targer leak causers
+# Drop leaky predictors from dataset
+potential_leaks = ['expenditure', 'share', 'active', 'majorcards']
+X2 = X.drop(potential_leaks, axis=1)
+
+# Evaluate the model with leaky predictors removed
+cv_scores = cross_val_score(my_pipeline, X2, y, 
+                            cv=5,
+                            scoring='accuracy')
+
+print("Cross-val accuracy: %f" % cv_scores.mean())
