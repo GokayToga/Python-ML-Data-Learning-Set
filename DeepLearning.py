04b98f1d3fd9956ffbdefb7b7c4f1d4b9de5517a
Gokay Toga <97601328+GokayToga@users.noreply.github.com>
2023-08-04 10:23:32 +0300

    Update DeepLearning.py

--- DeepLearning.py
+++ DeepLearning.py
@@ -140,3 +140,39 @@ early_stopping = EarlyStopping(
     patience=20, # how many epochs to wait before stopping
     restore_best_weights=True,
 )
+#example countinuing
+
+from tensorflow import keras
+from tensorflow.keras import layers, callbacks
+
+early_stopping = callbacks.EarlyStopping(
+    min_delta=0.001, # minimium amount of change to count as an improvement
+    patience=20, # how many epochs to wait before stopping
+    restore_best_weights=True,
+)
+
+model = keras.Sequential([
+    layers.Dense(512, activation='relu', input_shape=[11]),
+    layers.Dense(512, activation='relu'),
+    layers.Dense(512, activation='relu'),
+    layers.Dense(1),
+])
+model.compile(
+    optimizer='adam',
+    loss='mae',
+)
+
+#after creatin early stopping we add it to the fit function
+
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=256,
+    epochs=500,
+    callbacks=[early_stopping], # put your callbacks in a list
+    verbose=0,  # turn off training log
+)
+
+history_df = pd.DataFrame(history.history)
+history_df.loc[:, ['loss', 'val_loss']].plot();
+print("Minimum validation loss: {}".format(history_df['val_loss'].min()))
